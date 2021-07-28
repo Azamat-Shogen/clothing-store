28dd453a036292b500c09947d88a942121543904
baki-han <baki47hanma@gmail.com>
2021-07-27 22:53:23 -0500

    collection page added with collection items and styling

--- src/App.js
+++ src/App.js
@@ -2,15 +2,9 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from "./pages/homepage/homepage.components";
+import ShopPage from "./pages/shop/shop.component";
 
-const HatsPage = (props) => {
-    console.log(props)
-    return (
-        <div>
-            <h1>Hats Page</h1>
-        </div>
-    )
-}
+
 
 function App() {
 
@@ -18,7 +12,7 @@ function App() {
     <div >
         <Switch>
             <Route  exact path="/" component={Homepage} />
-            <Route  path="/hats" component={HatsPage} />
+            <Route  path="/shop" component={ShopPage} />
         </Switch>
     </div>
   );
