346b6a4a7c8649400b262a37c0387e0a974d78c2
baki-han <baki47hanma@gmail.com>
2021-07-29 13:42:21 -0500

    added header component and styles scss

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from "./pages/homepage/homepage.components";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 
 
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <div >
+        <Header />
         <Switch>
             <Route  exact path="/" component={Homepage} />
             <Route  path="/shop" component={ShopPage} />
